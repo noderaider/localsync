693ea1517ff33f849fb899a3aad1bd1e434a6f04
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-07 14:16:43 -0700

    esdocs fixed

--- src/lib/localsync.js
+++ src/lib/localsync.js
@@ -1,6 +1,16 @@
 import ls from 'local-storage'
 const should = require('chai').should()
 
+/**
+ * Creates a synchronizer which uses localStorage 'storage' event for synchronization between tabs.
+ * @param  {string}   key                       The key to synchronize on.
+ * @param  {function} action                    The action to run when trigger is executed. Should return the payload to be transmitted to the handlers on other tabs.
+ * @param  {function} handler                   The handler which is executed on other tabs when a synchronization is triggered. Argument is the return value of the action.
+ * @param  {boolean}  [options.tracing=false]   Option to turn on tracing for debugging.
+ * @param  {Object}   [options.logger=console]  The logger to debug to.
+ * @param  {string}   [options.logLevel=info]   The log level to trace at.
+ * @return {Object}                             localsync instance with start, stop, trigger, isRunning, and isFallback properties.
+ */
 export default function localsync(key, action, handler, { tracing = false, logger = console, logLevel = 'info' } = {}) {
   should.exist(key)
   should.exist(action)
