6d2746eaba547d84aa2836e062ac4d8ec58bdc66
Tom Chen <developer@tomchentw.com>
2017-01-26 11:38:30 +0800

    switch should to invariant

--- packages/serversync/src/lib/index.js
+++ packages/serversync/src/lib/index.js
@@ -1,4 +1,4 @@
-const should = require('chai').should()
+import invariant from 'invariant'
 const mechanism = 'serversync'
 
 /**
@@ -12,9 +12,9 @@ const mechanism = 'serversync'
  * @return {Object}                             serversync instance with start, stop, trigger, isRunning, and isFallback mock properties.
  */
 export default function serversync(key, action, handler, { tracing = false, logger = console, logLevel = 'info' } = {}) {
-  should.exist(key)
-  should.exist(action)
-  should.exist(handler)
+  invariant(key)
+  invariant(action)
+  invariant(handler)
   const log = (...args) => tracing ? logger[logLevel](...args) : () => {}
   let isRunning = false
 
