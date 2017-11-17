99183ae4f40f0ba3da69cda39859a1535dc5ac54
Cole Chamberlain <cochamberlain@google.com>
2017-11-17 05:55:30 -0800

    updated module def

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
-type LogLevel = "info" | "warn" | "error" | "silent";
+export as namespace Localsync;
 
-interface Opts {
+export type LogLevel = "info" | "warn" | "error" | "silent";
+
+export interface Opts {
     tracing: boolean;
     logger: Console;
     logLevel: LogLevel;
@@ -11,7 +13,7 @@ interface Opts {
     httpOnly: boolean;
 }
 
-interface LocalsyncNavigator {
+export interface LocalsyncNavigator {
     appName: string;
     appVersion?: string;
 }
